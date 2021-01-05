f3d6da4d5194b94f27410b3a89f7957fcdf67fac
dagp-ucol <dgentner@ucol.mx>
2021-01-04 18:10:43 -0600

    added Nombre&Apellido and deleted 'CLS'

--- Windows/Mayor de 2 numeros.bat
+++ Windows/Mayor de 2 numeros.bat
@@ -7,7 +7,6 @@ set/p numberA=Introduce el primer numero=
 set/p numberB=Introduce el segundo numero=
 echo.
 
-cls
 if %numberA% EQU %numberB% echo %numberB% y %numberA% son iguales.
 if %numberA% GTR %numberB% echo %numberA% es mas grande que %numberB%
 if %numberB% GTR %numberA% echo %numberB% es mas grande que %numberA%.
---
+++ Windows/Nombre y apellido alrevez.bat
@@ -0,0 +1,12 @@
+@echo off
+title Mayor de 2 numeros A o B
+color a
+
+echo.
+set/p nombre=Introduce el nombre=
+set/p apellido=Introduce el apellido=
+echo.
+
+echo %apellido% %nombre%
+
+pause
