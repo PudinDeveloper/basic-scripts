79363b343d87894f18c37167f5f2bf91ac49afbd
dagp-ucol <dgentner@ucol.mx>
2021-01-05 22:19:19 -0600

    Added suma de 3 edades

--- Linux/script.sh
+++ Linux/script.sh
@@ -1,3 +1,10 @@
-echo -n "Dame el 1er numero:"
-suma=$((1 + 2))
-echo $suma
+echo "Suma de 3 edades"
+
+echo -n "Dame la 1ra Edad: "
+read num1
+echo -n "Dame la 2da Edad: "
+read num2
+echo -n "Dame la 3ra Edad: "
+read num3
+suma=$(($num1 + $num2 + $num3))
+echo El resultado es $suma
