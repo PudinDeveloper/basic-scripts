5bc51d33378e9165e1f57d757044cacf6b92c6ae
dagp-ucol <dgentner@ucol.mx>
2021-01-05 22:44:03 -0600

    Added Mayor de 2 numeros

---
+++ Linux/Mayor de 2 numeros.sh
@@ -0,0 +1,18 @@
+echo "Mayor de 2 numeros"
+
+echo -n "Dame el 1er numero: "
+read num1
+echo -n "Dame el 2do numero: "
+read num2
+
+if [ $num1 -eq $num2 ]; then
+        echo "Son numeros iguales"
+fi
+
+if [ $num2 -gt $num1 ]; then
+        echo $num2 "es mayor"
+fi
+
+if [ $num1 -gt $num2 ]; then
+        echo $num1 "es mayor"
+fi
