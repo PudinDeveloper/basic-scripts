acf229694cff874200d5ca07feeee8e183290126
dagp-ucol <dgentner@ucol.mx>
2021-01-11 22:43:42 -0600

    Added Que equipo le vas

---
+++ Linux/Que equipo le vas.sh
@@ -0,0 +1,27 @@
+echo "Que equipo le vas"
+
+echo "Escribe 'America' 'Pumas' 'Monterrey' o 'Chivas':"
+read equipo
+
+# America
+if [ $equipo = America ]; then
+        echo "Tienes un aguila Interior"
+fi
+# Pumas
+if [ $equipo = Pumas ]; then
+        echo "Tienes un Puma Interior"
+fi
+# Monterrey
+if [ $equipo = Monterrey ]; then
+        echo "Tienes un Rayado Interior"
+fi
+# Chivas
+if [ $equipo = Chivas ]; then
+        echo "Tienes una Chiva Interior"
+fi
+
+#########################
+
+if [ $equipo != America -a $equipo != Pumas -a $equipo != Monterrey -a $equipo != Chivas ]; then
+        echo "No pos te Amolaste Brother"
+fi
